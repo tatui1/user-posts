e1207862fdc3fbf02795a3d8d19a7e854e32f3fd
tatui1 <kamila.lumazova@compasscollege.art>
2026-02-21 17:51:31 +0600

    Добавила UserCard

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,20 @@
+import type{ User } from '../../types'
+
+interface Props {
+  user: User
+  onClick: () => void
+  isSelected: boolean
+}
+
+export const UserCard = ({ user, onClick, isSelected }: Props) => {
+  return (
+    <div 
+      className={`user-card ${isSelected ? 'selected' : ''}`}
+      onClick={onClick}
+    >
+      <h3>{user.name}</h3>
+      <p>{user.email}</p>
+      <p>{user.company.name}</p>
+    </div>
+  )
+}
